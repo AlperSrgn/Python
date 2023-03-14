f192b007ca78d90a37346da5379910b338ae6776
Alper <alpersarginercun@gmail.com>
2023-03-14 16:18:23 +0300

    Update

---
+++ range-enum-zip.py
@@ -0,0 +1,37 @@
+print(range(10))        # [start:end]
+
+print(list(range(5,20,2)))
+
+print(*range(5,20,2))
+
+for i in range(10):
+    if i >5:
+        print(i)
+print()
+#***************************************************************************************************************
+
+harfler = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+for harf in harfler:
+    print(harf)
+
+for harf in enumerate(harfler):     #index no
+    print(harf)
+
+print()
+#***************************************************************************************************************
+
+ulkeler = ['TR','FR','DE']
+siraNo = range(1,4)
+
+for i in zip(ulkeler,siraNo):
+    print(i)
+
+print()
+#***************************************************************************************************************
+harfler = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+for harf in enumerate()
+
+
+
